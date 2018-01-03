dedad1d80244e6bae6273eeb2b25ed074a13e42c
sabrinasukhin <sabrina@sukhin.com>
2018-01-03 13:01:33 -0500

    Jump

---
+++ Independent-Study/Assets/CatMove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatMove : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    public float moveSpeed = 500f;
+    private float JUMP_FORCE = 250f;
+
+    // Update is called once per frame, use FixedUpdate when dealing with physics
+    public void jump()
+    {
+        rb.AddForce(Vector3.up * JUMP_FORCE);
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(moveSpeed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-moveSpeed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKeyDown("space") && gameObject.transform.position.y <= .6)
+        {
+            jump();
+        }
+    }
+}

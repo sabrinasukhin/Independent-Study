5c56bf7a82c5c3f98b28520d453b80e7e0ef3964
sabrinasukhin <sabrina@sukhin.com>
2018-01-04 23:45:11 -0500

    Broken bar

---
+++ Independent-Study/Assets/ChangeViews.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeViews : MonoBehaviour
+{
+    public float fullBar = 100.0f;
+    private float currentBar;
+    public float decreaseTime;
+    public float increaseTime;
+    public Slider viewBar;
+    Camera cam;
+    public Color alive;
+    public Color dead;
+    private bool isAlive;
+
+	void Start ()
+    {
+        currentBar = fullBar;
+        viewBar.value = CalcBar();
+        cam = GetComponent<Camera>();
+        isAlive = true;
+	}
+
+    float CalcBar()
+    {
+        return currentBar / fullBar;
+    }
+	
+	void Update ()
+    {
+        cam.backgroundColor = alive;
+        isAlive = true;
+        currentBar += increaseTime * Time.deltaTime;
+        CalcBar();
+
+        if (currentBar > 100)
+        {
+            currentBar = 100;
+        }
+
+        if (currentBar > 0)
+        {
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                currentBar -= decreaseTime * Time.deltaTime;
+                viewBar.value = CalcBar();
+                cam.backgroundColor = dead;
+                isAlive = false;
+            }
+        }
+	}
+}
